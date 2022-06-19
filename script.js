291bd48c5140d549964e3583ba12a0e79e385367
claudiomarquesprogramador <99271674+claudiomarquesprogramador@users.noreply.github.com>
2022-06-19 19:08:37 -0300

    nuvem colocada e adicionado botao menu

--- script.js
+++ script.js
@@ -6,24 +6,36 @@ function readImage1() {
         };       
         file.readAsDataURL(this.files[0]);
     }}
-    document.getElementById("img1-input").addEventListener("change", readImage1, false);
-    //primeira imagem terminada
-    function readImage2() {
-    if (this.files && this.files[0]) {
-        var file = new FileReader();
-        file.onload = function(e) {
-        document.getElementById("imagem2").src = e.target.result;
-        };       
-        file.readAsDataURL(this.files[0]);
-    }}
-    document.getElementById("img2-input").addEventListener("change", readImage2, false);
-    //primeira imagem terminada
-    function readImage3() {
-    if (this.files && this.files[0]) {
-        var file = new FileReader();
-        file.onload = function(e) {
-        document.getElementById("imagem3").src = e.target.result;
-        };       
-        file.readAsDataURL(this.files[0]);
-    }}
-    document.getElementById("img3-input").addEventListener("change", readImage3, false);
+document.getElementById("img1-input").addEventListener("change", readImage1, false);
+//primeira imagem terminada
+function readImage2() {
+if (this.files && this.files[0]) {
+    var file = new FileReader();
+    file.onload = function(e) {
+    document.getElementById("imagem2").src = e.target.result;
+    };       
+    file.readAsDataURL(this.files[0]);
+}}
+document.getElementById("img2-input").addEventListener("change", readImage2, false);
+//segunda imagem terminada
+function readImage3() {
+if (this.files && this.files[0]) {
+    var file = new FileReader();
+    file.onload = function(e) {
+    document.getElementById("imagem3").src = e.target.result;
+    };       
+    file.readAsDataURL(this.files[0]);
+}}
+document.getElementById("img3-input").addEventListener("change", readImage3, false);
+//terceira imagem terminada
+let visibilidade = true;
+function displaymenu() {
+    if(visibilidade){
+        document.getElementById("form1").style.display="none";
+        visibilidade=false;
+    }else{
+        document.getElementById("form1").style.display="flex";
+        visibilidade=true;
+    }   
+}
+//terminei visibilidade dos input das fotos
