fc0e6d204b3537b58ee3652fc41ccf7fd4f30a76
claudiomarquesprogramador <99271674+claudiomarquesprogramador@users.noreply.github.com>
2022-06-21 20:30:49 -0300

    botao alterar colocado

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
+//adicionando imagens apertando o botão Criar Imagens
+const apikey="563492ad6f917000010000019109678cff424cc785b4cb62e961147e";
+let page_num=1;
+function criarImagem(page_num) {
+    const data= fetch(`https://api.pexels.com/v1/curated?page=${page_num}`,
+        {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization: apikey,
+        },
+    }).then(res => res.json()).then(data => {
+        document.getElementById("imagem1").src = data.photos[0].src.medium;
+        document.getElementById("imagem2").src = data.photos[1].src.medium;
+        document.getElementById("imagem3").src = data.photos[2].src.medium;
+    });
+};
+//final
 
+//alterar imagens apertando o botão Alterar
+function alterarImagem() {
+    page_num++;
+    criarImagem(page_num);
+};
+//final
 
-
-
-
-
+//adicionar primeira imagem
 function readImage1() {
     if (this.files && this.files[0]) {
         var file = new FileReader();
@@ -13,8 +34,9 @@ function readImage1() {
         file.readAsDataURL(this.files[0]);
     }}
 document.getElementById("img1-input").addEventListener("change", readImage1, false);
-//primeira imagem terminada
+//final
 
+//adicionar segunda imagem
 function readImage2() {
 if (this.files && this.files[0]) {
     var file = new FileReader();
@@ -24,8 +46,9 @@ if (this.files && this.files[0]) {
     file.readAsDataURL(this.files[0]);
 }}
 document.getElementById("img2-input").addEventListener("change", readImage2, false);
-//segunda imagem terminada
+//final
 
+//adicionar terceira imagem
 function readImage3() {
 if (this.files && this.files[0]) {
     var file = new FileReader();
@@ -35,8 +58,9 @@ if (this.files && this.files[0]) {
     file.readAsDataURL(this.files[0]);
 }}
 document.getElementById("img3-input").addEventListener("change", readImage3, false);
-//terceira imagem terminada
+//final
 
+//mudar visibilidade do header com os input das fotos e texto
 let visibilidade = true;
 function displaymenu() {
     if(visibilidade){
@@ -47,8 +71,9 @@ function displaymenu() {
         visibilidade=true;
     }   
 }
-//terminei visibilidade dos input das fotos
+//final
 
+//adicionar o primeiro texto
 let captar1 = "";
 function textoEnviado1() {
     captar1 = document.getElementById("texto1").value;
@@ -59,8 +84,9 @@ function textoEnviado1() {
         document.getElementById("p1").innerHTML = captar1;
     }
 }
-//terminado texto1
+//final
 
+//adicionar o segundo texto
 let captar2 = "";
 function textoEnviado2() {
     captar2 = document.getElementById("texto2").value;
@@ -71,8 +97,9 @@ function textoEnviado2() {
         document.getElementById("p2").innerHTML = captar2;
     }
 }
-//terminado texto2
+//final
 
+//adicionar o terceiro texto
 let captar3 = "";
 function textoEnviado3() {
     captar3 = document.getElementById("texto3").value;
@@ -83,4 +110,4 @@ function textoEnviado3() {
         document.getElementById("p3").innerHTML = captar3;
     }
 }
-//terminado texto3
+//final
